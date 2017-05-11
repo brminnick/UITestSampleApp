50f7c0dec6723773704770df1933997a7b94f397
brminnick <brandon.r.minnick@gmail.com>
2017-05-11 11:50:28 -0700

    Updated Login_iOS

--- UITestSampleApp/UITestSampleApp.iOS/Services/Login_iOS.cs
+++ UITestSampleApp/UITestSampleApp.iOS/Services/Login_iOS.cs
@@ -9,42 +9,38 @@ using UITestSampleApp.iOS;
 [assembly: Dependency(typeof(Login_iOS))]
 namespace UITestSampleApp.iOS
 {
-	public class Login_iOS : ILogin
-	{
+    public class Login_iOS : ILogin
+    {
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        public async Task<bool> SetPasswordForUsername(string username, string password)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        public Task<bool> SetPasswordForUsername(string username, string password)
         {
-			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-				return false;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return Task.FromResult(false);
 
-			KeychainHelpers.SetPasswordForUsername(username, password, "XamarinExpenses", Security.SecAccessible.Always, true);
-			NSUserDefaults.StandardUserDefaults.SetString(username, "username");
-			NSUserDefaults.StandardUserDefaults.SetBool(true, "hasLogin");
-			NSUserDefaults.StandardUserDefaults.Synchronize();
+            KeychainHelpers.SetPasswordForUsername(username, password, "XamarinExpenses", Security.SecAccessible.Always, true);
+            NSUserDefaults.StandardUserDefaults.SetString(username, "username");
+            NSUserDefaults.StandardUserDefaults.SetBool(true, "hasLogin");
+            NSUserDefaults.StandardUserDefaults.Synchronize();
 
-			return true;
-		}
+            return Task.FromResult(true);
+        }
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        public async Task<bool> CheckLogin(string username, string password)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        public Task<bool> CheckLogin(string username, string password)
         {
-			var _username = NSUserDefaults.StandardUserDefaults.ValueForKey(new NSString("username"));
-			var _password = KeychainHelpers.GetPasswordForUsername(username, "XamarinExpenses", true);
+            var _username = NSUserDefaults.StandardUserDefaults.ValueForKey(new NSString("username"));
+            var _password = KeychainHelpers.GetPasswordForUsername(username, "XamarinExpenses", true);
 
-			if (_username == null || _password == null)
-				return false;
+            if (_username == null || _password == null)
+                return Task.FromResult(false);
 
-			if (password == _password &&
-				username == _username.ToString())
-			{
-				return true;
-			}
+            if (password == _password &&
+                username == _username.ToString())
+            {
+                return Task.FromResult(true);
+            }
 
-			return false;
-		}
-	}
+            return Task.FromResult(false);
+        }
+    }
 }
 
