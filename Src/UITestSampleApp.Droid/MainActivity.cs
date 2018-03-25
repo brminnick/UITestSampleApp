3a5a04ec56e47966fa6c4d461ceda6637deb5017
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-24 17:42:56 -0700

    Added FirebaseApp.InitializeApp

--- Src/UITestSampleApp.Droid/MainActivity.cs
+++ Src/UITestSampleApp.Droid/MainActivity.cs
@@ -5,54 +5,59 @@ using Android.Content.PM;
 
 using Akavache;
 
+using Firebase;
+
 using Java.Interop;
+
+using Plugin.CurrentActivity;
 
 using Xamarin.Forms.Platform.Android.AppLinks;
 
 namespace UITestSampleApp.Droid
 {
-    [Activity(Theme = "@style/MyTheme", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        #region Methods
-        #region Xamarin Test Cloud Back Door Methods
+	[Activity(Theme = "@style/MyTheme", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		#region Methods
+		#region Xamarin Test Cloud Back Door Methods
 #if DEBUG
-        [Export("BypassLoginScreen")]
-        public void BypassLoginScreen() =>
-            BackdoorMethodHelpers.BypassLoginScreen();
+		[Export("BypassLoginScreen")]
+		public void BypassLoginScreen() =>
+			BackdoorMethodHelpers.BypassLoginScreen();
 
-        [Export("OpenListViewPage")]
-        public void OpenListViewPage() =>
-            BackdoorMethodHelpers.OpenListViewPage();
+		[Export("OpenListViewPage")]
+		public void OpenListViewPage() =>
+			BackdoorMethodHelpers.OpenListViewPage();
 
-        [Export("GetListViewPageDataAsBase64String")]
-        public string GetListViewPageDataAsBase64String() =>
-            BackdoorMethodHelpers.GetListViewPageDataAsBase64String();
+		[Export("GetListViewPageDataAsBase64String")]
+		public string GetListViewPageDataAsBase64String() =>
+			BackdoorMethodHelpers.GetListViewPageDataAsBase64String();
 #endif
-        #endregion
+		#endregion
 
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
+			FirebaseApp.InitializeApp((Android.Content.Context)CrossCurrentActivity.Current);
 
-            TabLayoutResource = Resource.Layout.tabs;
-            ToolbarResource = Resource.Layout.toolbar;
+			TabLayoutResource = Resource.Layout.tabs;
+			ToolbarResource = Resource.Layout.toolbar;
 
-            global::Xamarin.Forms.Forms.SetFlags("FastRenderers_Experimental");
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            AndroidAppLinks.Init(this);
+			global::Xamarin.Forms.Forms.SetFlags("FastRenderers_Experimental");
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			AndroidAppLinks.Init(this);
 
-            BlobCache.ApplicationName = "SimpleUITestApp";
-            BlobCache.EnsureInitialized();
+			BlobCache.ApplicationName = "SimpleUITestApp";
+			BlobCache.EnsureInitialized();
 
-            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
-            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
-            EntryCustomReturn.Forms.Plugin.Android.CustomReturnEntryRenderer.Init();
+			Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+			EntryCustomReturn.Forms.Plugin.Android.CustomReturnEntryRenderer.Init();
 
-            LoadApplication(new App());
-        }
-        #endregion
-    }
+			LoadApplication(new App());
+		}
+		#endregion
+	}
 }
 
