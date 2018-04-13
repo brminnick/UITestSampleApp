00684f6e4a9447d24e955d3310fc4c6b206e30d6
Brandon Minnick <brminnick@users.noreply.github.com>
2018-04-13 15:50:09 -0700

    Fixed post build script

--- Src/UITestSampleApp.Droid/appcenter-post-build.sh
+++ Src/UITestSampleApp.Droid/appcenter-post-build.sh
@@ -18,10 +18,10 @@ if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
 
     msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
 
-    UITestDLL=`find "$APPCENTER_OUTPUT_DIRECTORY" -name "UITestSampleApp.UITests.dll" | grep bin`
+    UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "UITestSampleApp.UITests.dll" | grep bin`
     UITestBuildDir=`dirname $UITestDLL`
 
-    APKFile=`find "$APPCENTER_OUTPUT_DIRECTORY" -name *.apk | head -1`
+    APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
 
     npm install -g appcenter-cli
 
