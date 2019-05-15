00488719f486d4c7ee0a24c583e2bfc134ba520e
Brandon Minnick <brminnick@users.noreply.github.com>
2019-05-15 16:22:24 -0700

    Added Unit Tests

---
+++ Src/UITestSampleApp.UnitTests/Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using UITestSampleApp.Shared;
+namespace UITestSampleApp.UnitTests
+{ 
+    public class Test
+    {
+        [Test]
+        public void ListPageDataModelTest()
+        {
+            //Arrange
+            const int detail = 7;
+            const string text = "Test";
+            var model = new ListPageDataModel();
+
+            //Act
+            model.Detail = detail;
+            model.Text = text;
+
+            //Assert
+            Assert.IsNotNull(model.Id);
+            Assert.AreEqual(detail, model.Detail);
+            Assert.AreEqual(text, model.Text);
+        }
+    }
+}
