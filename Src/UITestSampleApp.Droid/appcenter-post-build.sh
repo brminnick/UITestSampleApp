14cff462a37e90baded77736f42fdfff7d96c72b
Brandon Minnick <brminnick@users.noreply.github.com>
2019-05-15 17:11:29 -0700

    Revert Merge

--- Src/UITestSampleApp.Droid/appcenter-post-build.sh
+++ Src/UITestSampleApp.Droid/appcenter-post-build.sh
@@ -2,7 +2,7 @@
 if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
 
     echo "Post Build Script Started"
-    
+
     SolutionFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name UITestSampleApp.sln`
     SolutionFileFolder=`dirname $SolutionFile`
 
@@ -21,7 +21,7 @@ if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
 
     UITestBuildDir=`dirname $UITestDLL`
     echo UITestBuildDir: $UITestBuildDir
-     
+
     UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
     echo UITestVersionNumber: $UITestVersionNumber
 
@@ -31,15 +31,12 @@ if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
     TestCloudExeDirectory=`dirname $TestCloudExe`
     echo TestCloudExeDirectory: $TestCloudExeDirectory
 
-    IPAFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.ipa | head -1`
-    echo IPAFile: $IPAFile
-
-    DSYMFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.dSYM | head -1`
-    echo DSYMFile: $DSYMFile
+    APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
+    echo APKFile: $APKFile
 
     npm install -g appcenter-cli
 
     appcenter login --token $AppCenterAPIToken
 
-    appcenter test run uitest --app "bminnick/UITestSampleApp-1" --devices "bminnick/ios10-plus" --app-path $IPAFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --dsym-dir $DSYMFile --uitest-tools-dir $TestCloudExeDirectory --async
+    appcenter test run uitest --app "bminnick/uitestsampleapp" --devices "bminnick/all-supported-os-versions" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --uitest-tools-dir $TestCloudExeDirectory --async
 fi
