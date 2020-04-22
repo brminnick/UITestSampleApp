f51fe8f29f1a700ea0b40c837f7ad9418dd83635
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-04-22 08:54:40 -0700

    Revert AppCenter to v2.6.4

--- Src/UITestSampleApp.Shared/Models/ListPageDataModel.cs
+++ Src/UITestSampleApp.Shared/Models/ListPageDataModel.cs
@@ -5,7 +5,13 @@ namespace UITestSampleApp.Shared
 {
     public class ListPageDataModel
     {
-        public ListPageDataModel(int detail, string text) : this(detail,text,Guid.NewGuid().ToString())
+        [JsonIgnore]
+        public const string CollectionId = "uitestsampleapp-collection";
+
+        [JsonIgnore]
+        public const string DatabaseId = "uitestsampleapp-database";
+
+        public ListPageDataModel(int detail, string text) : this(detail, text, Guid.NewGuid().ToString())
         {
 
         }
@@ -13,12 +19,6 @@ namespace UITestSampleApp.Shared
         [JsonConstructor]
         public ListPageDataModel(int detail, string text, string id) =>
             (Detail, Text, Id) = (detail, text, id);
-
-        [JsonIgnore]
-        public const string CollectionId = "uitestsampleapp-collection";
-
-        [JsonIgnore]
-        public const string DatabaseId = "uitestsampleapp-database";
 
         public string Id { get; }
         public int Detail { get; }
