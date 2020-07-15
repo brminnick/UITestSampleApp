5ea3f8c2ff9aa9fc93e5494c1159c09301719524
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-07-15 10:33:34 -0700

    Update NuGet Packages

--- Src/UITestSampleApp/ViewModels/Base/BaseViewModel.cs
+++ Src/UITestSampleApp/ViewModels/Base/BaseViewModel.cs
@@ -29,6 +29,6 @@ namespace UITestSampleApp
         }
 
         void OnPropertyChanged([CallerMemberName]in string name = "") =>
-            _propertyChangedEventManager.HandleEvent(this, new PropertyChangedEventArgs(name), nameof(INotifyPropertyChanged.PropertyChanged));
+            _propertyChangedEventManager.RaiseEvent(this, new PropertyChangedEventArgs(name), nameof(INotifyPropertyChanged.PropertyChanged));
     }
 }
