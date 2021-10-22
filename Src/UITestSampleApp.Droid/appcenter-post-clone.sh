d8ae5d1b480eb24091cd06016b060b480eead26c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-22 12:53:48 -0400

    Update appcenter-post-clone.sh

--- Src/UITestSampleApp.Droid/appcenter-post-clone.sh
+++ Src/UITestSampleApp.Droid/appcenter-post-clone.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh --version $netcoreversion --install-dir "$AGENT_TOOLSDIRECTORY/dotnet"
+
 dotnet restore **/**.sln
